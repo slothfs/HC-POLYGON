6544d0e6e2c4e10dd50df104346e12d7b0fe3afd
AK3 <mjlabishek123@gmail.com>
2026-05-21 22:08:10 +0545

    added a ring hurrey

---
+++ 3d project/src/main.js
@@ -0,0 +1,42 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+}); 
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const geometry = new THREE.TorusGeometry(10, 3, 100, 100);
+const material = new THREE.MeshStandardMaterial({ 
+  color: 0xffffff,
+  wireframe: true
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+  requestAnimationFrame( animate);
+  renderer.render( scene, camera);
+}
+
+animate()
